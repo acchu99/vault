d0782f6a1cd76891deed10591a8fac3aa901268e
Ashwin Balasubramanian <acchu99@gmail.com>
2024-08-23 17:09:26 -0700

    pwa trial

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+const nextConfig = withPWA({
     images: {
         remotePatterns: [
             {
@@ -10,6 +15,6 @@ const nextConfig = {
             },
         ],
     },
-};
+});
 
 export default nextConfig;
