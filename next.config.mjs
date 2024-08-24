91baee9bc171f0e679b47b2baf0e6430beaf139c
Ashwin Balasubramanian <acchu99@gmail.com>
2024-08-23 17:11:10 -0700

    module

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+import withPWA from 'next-pwa';
 
 const nextConfig = withPWA({
     images: {
